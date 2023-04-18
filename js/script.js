a715ceecf5a4daf1cba1a4a01efea10c25393831
Muhammad <muhammadibrohimov0306@gmail.com>
2023-04-18 14:34:12 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const image = document.querySelector('img'),
+     input = document.querySelector("input");
+
+     input.addEventListener('change', () =>{
+        image.src = URL.createObjectURL(input.files[0]);
+     });
+
+   
